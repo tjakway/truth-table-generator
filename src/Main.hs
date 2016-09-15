459b0e1d6150d80b46ff38626b7e0e33f7dfaa61
Thomas Jakway <tjakway@nyu.edu>
2016-09-15 11:48:44 -0400

    reorganized statement tree

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,21 @@
 module Main where
 
-import Data.Map
+import Data.Map.Strict (lookup)
+
+-- | lookup or die with an informative error
+-- basically fromJust with a bad excuse for diagnostics
+lookupOrFail item mapping = case lookup item mapping of 
+                                      Just x  -> x
+                                      Nothing -> error ("Lookup of " ++ (show item) ++ " in " ++ (show mapping) ++ " failed!")
 
 --Unbiased Either type
+--couldn't find an implementation of this within 5 minutes of googling
 data OneOf a b = Up a | Down b
 
-oneOf :: (a -> c) -> (b -> d) -> OneOf a b -> OneOf c d
-oneOf f g (Up a)   = Up   (f a)
-oneOf f g (Down b) = Down (g b)
-
+--analogous to the function either
+forOneOf :: (a -> c) -> (b -> d) -> OneOf a b -> OneOf c d
+forOneOf f g (Up a)   = Up   (f a)
+forOneOf f g (Down b) = Down (g b)
 
 data Operator = And
               | Or
@@ -17,10 +24,24 @@ data Operator = And
 newtype Variable = Variable String
 
 data Statement = Negation Statement
+               | VariableStatement Variable
+               -- ^ a statement that just wraps a variable so we can negate
+               -- variables without further work
                | Statement (OneOf Statement Variable) Operator (OneOf Statement Variable)
 
-evaluate :: Map Variable Bool -> Statement -> Bool
-evaluate vars (Negation stmt) = not . evaluate vars $ stmt
+evaluateStatement :: Map Variable Bool -> Statement -> Bool
+evaluateStatement vars (Negation stmt) = not . evaluateStatement vars $ stmt
+evaluateStatement vars (VariableStatement thisVar) = 
+evaluateStatement vars (Statement (Up stmt) op _) = evaluateStatement vars stmt
+evaluateStatement vars (Statement _ op (Up stmt)) = evaluateStatement vars stmt
+evaluateStatement vars (Statement (Down first) op (Down second)) = evaluateOperator op first second
+
+evaluateOperator :: Operator -> Bool -> Bool -> Bool
+evaluateOperator op first second 
+                    | op == And = first && second
+                    | op == Or  = first || second
+                    | op == Xor = (first || second) && (first /= second)
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
