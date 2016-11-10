9baca910ed8010be4d934c1b3522a6ce53dafd11
Thomas Jakway <tjakway@nyu.edu>
2016-11-10 14:36:40 -0500

    working on evaluateStatement

--- src/TruthTable/Logic.hs
+++ src/TruthTable/Logic.hs
@@ -9,18 +9,19 @@ lookupEither k m = case Map.lookup k m of Just r  -> Right r
                                           Nothing -> Left k
 
 evaluateStatement :: Map.Map Variable Bool -> Statement -> Either Variable Bool
+evaluateStatement vars (StatementResult b) = Right b
 evaluateStatement vars (NestedStatement stmt) = evaluateStatement vars stmt
-evaluateStatement vars (NegationStatement stmt) = fmap not $ evaluateStatement vars $ stmt
-evaluateStatement vars (VariableStatement thisVar) = lookupEither thisVar vars
--- ^ if this statement just wraps a variable, just look up its value in the table
-evaluateStatement vars (Statement (NestedStatement stmt) op _) = evaluateStatement vars stmt
-evaluateStatement vars (Statement _ op (NestedStatement stmt)) = evaluateStatement vars stmt
 evaluateStatement vars (Statement (VariableStatement first) op (VariableStatement second)) = 
         let find x = lookupEither x vars in
             find first >>= \f ->
             find second >>= \s -> 
             return $ evaluateOperator op f s
-
+evaluateStatement vars (NegationStatement stmt) = fmap not $ evaluateStatement vars $ stmt
+evaluateStatement vars (VariableStatement thisVar) = lookupEither thisVar vars
+-- ^ if this statement just wraps a variable, just look up its value in the table
+evaluateStatement vars s = let eval = fmap StatementResult $ evaluateStatement vars in
+    case s of (Statement (NestedStatement stmt) op x) -> (eval stmt) >>= \res -> eval (Statement res op x)
+              (Statement x op (NestedStatement stmt)) -> (eval stmt) >>= eval $ Statement x op 
 
 evaluateOperator :: Operator -> Bool -> Bool -> Bool
 evaluateOperator op first second 
