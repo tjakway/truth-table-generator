647d64a08951a8dd68fa388a2926cae29d70d105
Thomas Jakway <tjakway@nyu.edu>
2016-11-10 14:27:13 -0500

    changed type of evaluateStatement

--- src/TruthTable/Logic.hs
+++ src/TruthTable/Logic.hs
@@ -1,26 +1,26 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module TruthTable.Logic where
 
 import TruthTable.OneOf
 import TruthTable.Types
 import qualified Data.Map.Strict as Map 
 
+lookupEither :: Ord k => k -> Map.Map k v -> Either k v
+lookupEither k m = case Map.lookup k m of Just r  -> Right r
+                                          Nothing -> Left k
 
 evaluateStatement :: Map.Map Variable Bool -> Statement -> Either Variable Bool
 evaluateStatement vars (NestedStatement stmt) = evaluateStatement vars stmt
-evaluateStatement vars (NegationStatement stmt) = not . evaluateStatement vars $ stmt
+evaluateStatement vars (NegationStatement stmt) = fmap not $ evaluateStatement vars $ stmt
 evaluateStatement vars (VariableStatement thisVar) = lookupEither vars thisVar
 -- ^ if this statement just wraps a variable, just look up its value in the table
 evaluateStatement vars (Statement (NestedStatement stmt) op _) = evaluateStatement vars stmt
 evaluateStatement vars (Statement _ op (NestedStatement stmt)) = evaluateStatement vars stmt
 evaluateStatement vars (Statement (VariableStatement first) op (VariableStatement second)) = 
-        let find = lookupOrFail vars in
-            evaluateOperator op (find first) (find second)
+        let find = lookupEither vars in
+            find first >>= \f ->
+            find second >>= \s -> 
+            return $ evaluateOperator op f s
 
-    where lookupEither :: Ord k => k -> Map.Map k v -> Either k v
-          lookupEither k m = case Map.lookup k m of Just r  -> Right r
-                                                    Nothing -> Left k
 
 evaluateOperator :: Operator -> Bool -> Bool -> Bool
 evaluateOperator op first second 
