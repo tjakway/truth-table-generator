50fec5d9b24ce8f7c1078414e27b7a804361bf58
Thomas Jakway <tjakway@nyu.edu>
2016-11-10 14:29:34 -0500

    typechecks

--- src/TruthTable/Logic.hs
+++ src/TruthTable/Logic.hs
@@ -11,12 +11,12 @@ lookupEither k m = case Map.lookup k m of Just r  -> Right r
 evaluateStatement :: Map.Map Variable Bool -> Statement -> Either Variable Bool
 evaluateStatement vars (NestedStatement stmt) = evaluateStatement vars stmt
 evaluateStatement vars (NegationStatement stmt) = fmap not $ evaluateStatement vars $ stmt
-evaluateStatement vars (VariableStatement thisVar) = lookupEither vars thisVar
+evaluateStatement vars (VariableStatement thisVar) = lookupEither thisVar vars
 -- ^ if this statement just wraps a variable, just look up its value in the table
 evaluateStatement vars (Statement (NestedStatement stmt) op _) = evaluateStatement vars stmt
 evaluateStatement vars (Statement _ op (NestedStatement stmt)) = evaluateStatement vars stmt
 evaluateStatement vars (Statement (VariableStatement first) op (VariableStatement second)) = 
-        let find = lookupEither vars in
+        let find x = lookupEither x vars in
             find first >>= \f ->
             find second >>= \s -> 
             return $ evaluateOperator op f s
