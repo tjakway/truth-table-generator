0c2b6327abd8224585997582503a8ecfd7e44074
Thomas Jakway <tjakway@nyu.edu>
2016-11-12 09:53:02 -0500

    began writing error checking

---
+++ src/TruthTable/ErrorChecking.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module TruthTable.ErrorChecking 
+( ErrorType(..),
+  validateTruthTable
+  )
+where
+
+import qualified Data.Map.Strict as Map 
+import TruthTable.Types
+
+data ErrorType = VariableLengthError [TruthSet]
+                    -- ^ all failing TruthSets
+               | NonuniqueTruthValueError [TruthSet]
+                    -- ^ all nonunique TruthSets
+               | WrongNumberOfTruthSetsError Int Int
+                    -- ^ Expected and Actual
+                    deriving (Show)
+
+validateTruthTable :: TruthTable -> Either ErrorType TruthTable
+validateTruthTable truthTable = undefined
+    where numVariables :: Int
+          numVariables = length . variables $ truthTable
+
+
+checkVariableLengthError :: TruthTable -> [TruthSet]
+checkVariableLengthError truthTable = 
+        reverse .
+        foldr (\thisTruthSet failingSets -> if Map.size thisTruthSet /= numVariables 
+            then thisTruthSet : failingSets
+            else failingSets ) [] 
+            . truthSets $ truthTable
+
+    where numVariables :: Int
+          numVariables = length . variables $ truthTable
+
+showError :: ErrorType -> String
+showError = show -- XXX: implement
