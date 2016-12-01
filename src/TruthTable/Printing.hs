0b471883cd56043a9bcd15f05e123e4153e6078c
Thomas Jakway <tjakway@nyu.edu>
2016-12-01 15:38:01 -0500

    wrote printRow

--- src/TruthTable/Printing.hs
+++ src/TruthTable/Printing.hs
@@ -11,6 +11,8 @@ data PrintConfig = PrintConfig {
 
 type Printer = State (PrintConfig, TruthTable)
 
+-- | take a row from the TruthTable, return it, and update state to reflect
+-- this
 takeRow :: Printer (TruthSet, Bool)
 takeRow = do
         (conf, truthTable) <- get
@@ -22,13 +24,31 @@ takeRow = do
         put (conf, newTruthTable)
         return (thisTruthSet, thisResult)
 
+getConfig :: Printer PrintConfig
+getConfig = get >>= return . fst
+
+printBool :: PrintConfig -> Bool -> String
+printBool conf True = trueString conf
+printBool conf False = falseString conf
 
 printRow :: Printer String
-printRow = undefined
+printRow = do
+        conf <- getConfig
+        (truthSet, result) <- takeRow
+
+        let pBool = printBool conf
+            delim = delimiter conf
+
+        -- TODO: consider printing the first item separately so the line
+        -- doesn't start with a delimiter
+        let rowStr = foldr (\thisTruthValue acc -> acc ++ delim ++ pBool thisTruthValue) "" truthSet
+        
+        -- don't forget to print the result as the last column
+        return $ rowStr ++ delim ++ pBool result
+
 
 printHeader :: Printer String
 printHeader = undefined
-
 
 printM :: Printer String
 printM = undefined
